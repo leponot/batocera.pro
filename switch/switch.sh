97bc5cafea98b8943e977798a734c2e84d5d1e8d
leponot <leponot@gmail.com>
2025-04-19 09:28:36 +0200

    Update switch.sh

--- switch/switch.sh
+++ switch/switch.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash 
 
-
-# Display the dialog message box
-dialog --msgbox "Installer is offline, due to Nintendo DMCA GitHub removals." 7 50
-
-# Clear the dialog remnants from the terminal screen
-clear
-
-# End the script
-exit 0
-
 # BATOCERA.PRO INSTALLER
 ######################################################################
 #--------------------------------------------------------------------- 
